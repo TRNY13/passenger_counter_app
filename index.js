e053ea9e9a77a1a6c304026c90a481385f64c20e
Titus Rainey <titusrainey@gmail.com>
2021-11-13 16:41:44 -0500

    created files and connected

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 // this variable will store the count of passengers in the app 
 
 // grabbing count-el from html and storing the info into js
-let countEl = document.getElementById("count-el")
+let countEl = document.getElementById("count-el");
 
-console.log(countEl)
+console.log(countEl);
 
 let count = 0;
 
 // this function will be used to count up the passengers in the app
 function increment(){
-count = count + 1
+count += 1
 // setting the inner text of the HTML to be equal to "count"
-countEl.innerText = count
+countEl.innerText = count;
 }
+
+// grabbing save-el from the HTML
+let saveEl = document.getElementById("save-el");
+
+function save() {
+ let logs = (count + " - ");
+ console.log(count );
+ // saveEl.innerText += <----- this needs to be += instead of just 
+ // equal. Without it being += the entirety of the inner text is
+ // changed instead of being added to with +=
+ saveEl.innerText += logs;
+}
+
