37ebd11704c229b45669f884c406b4b072b0158c
Titus Rainey <titusrainey@gmail.com>
2021-11-13 17:21:20 -0500

    created files and connected

--- index.js
+++ index.js
@@ -11,18 +11,25 @@ let count = 0;
 function increment(){
 count += 1
 // setting the inner text of the HTML to be equal to "count"
-countEl.innerText = count;
+countEl.textContent = count;
 }
 
 // grabbing save-el from the HTML
 let saveEl = document.getElementById("save-el");
 
 function save() {
- let logs = (count + " - ");
- console.log(count );
+ let logs = (" " + count + " - ");
+ console.log(count);
  // saveEl.innerText += <----- this needs to be += instead of just 
  // equal. Without it being += the entirety of the inner text is
  // changed instead of being added to with +=
- saveEl.innerText += logs;
+ // changed inner text to textContent to better display the saved logs on the webpage
+ // innertext also uses a lot of resources compared to textContent
+ saveEl.textContent += logs;
+ // These two lines reset the count in the console and the display
+ // of the count on the DOM
+ countEl.textContent = 0
+ count = 0
 }
+
 
